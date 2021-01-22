c340e13d86b69af55aa2dd33ddbedaca267f9a5d
wiasliaw <leewei48750@gmail.com>
2021-01-22 22:44:37 +0800

    style(.editorconfig): styling

--- leetcode/2_add_two_number/main.c
+++ leetcode/2_add_two_number/main.c
@@ -3,36 +3,38 @@
 
 struct ListNode
 {
-  int val;
-  struct ListNode *next;
+    int val;
+    struct ListNode *next;
 };
 
 void push(struct ListNode **head, int new_value)
 {
-  struct ListNode **indirect = head;
+    struct ListNode **indirect = head;
 
-  struct ListNode *new_node = malloc(sizeof(struct ListNode));
-  new_node->val = new_value;
-  new_node->next = NULL;
+    struct ListNode *new_node = malloc(sizeof(struct ListNode));
+    new_node->val = new_value;
+    new_node->next = NULL;
 
-  assert(new_node);
-  while (*indirect)
-    indirect = &(*indirect)->next;
-  *indirect = new_node;
+    assert(new_node);
+    while (*indirect)
+        indirect = &(*indirect)->next;
+    *indirect = new_node;
 }
 
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
-  struct ListNode *ptr = NULL;
-  int carry = 0;
-  for (struct ListNode *a = l1, *b = l2; a || b; a = a ? a->next : NULL, b = b ? b->next: NULL) {
-    int v1 = a ? a->val : 0;
-    int v2 = b ? b->val : 0;
-    push(&ptr, (v1+v2+carry)%10);
-    carry = ((v1+v2+carry)>=10) ? 1 :0;
-  }
-  if (carry == 1) {
-    push(&ptr, carry);
-  }
-  return ptr;
+    struct ListNode *ptr = NULL;
+    int carry = 0;
+    for (struct ListNode *a = l1, *b = l2; a || b; a = a ? a->next : NULL, b = b ? b->next : NULL)
+    {
+        int v1 = a ? a->val : 0;
+        int v2 = b ? b->val : 0;
+        push(&ptr, (v1 + v2 + carry) % 10);
+        carry = ((v1 + v2 + carry) >= 10) ? 1 : 0;
+    }
+    if (carry == 1)
+    {
+        push(&ptr, carry);
+    }
+    return ptr;
 }
--- leetcode/2_add_two_number/main.rs
+++ leetcode/2_add_two_number/main.rs
@@ -1,81 +1,81 @@
 #[derive(PartialEq, Eq, Clone, Debug)]
 struct ListNode {
-  val: i32,
-  next: Option<Box<ListNode>>
+    val: i32,
+    next: Option<Box<ListNode>>
 }
 
 impl ListNode {
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
     }
-  }
 }
 
 #[derive(Debug)]
 struct SinglyLinkedList {
-  head: Option<Box<ListNode>>
+    head: Option<Box<ListNode>>
 }
 
 impl SinglyLinkedList {
-  fn new() -> Self {
-    Self { head: None }
-  }
+    fn new() -> Self {
+        Self { head: None }
+    }
 
-  fn push(&mut self, val: i32) {
-    let next = self.head.take();
-    self.head = Some(Box::new(ListNode { val, next }));
-  }
+    fn push(&mut self, val: i32) {
+        let next = self.head.take();
+        self.head = Some(Box::new(ListNode { val, next }));
+    }
 }
 
 struct Solution{}
 
 impl Solution {
-  fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut ptr = SinglyLinkedList::new();
-    let mut v = Vec::new();
-    let mut a = l1.as_deref().take();
-    let mut b = l2.as_deref().take();
-    let mut c: i32 = 0;
-    loop {
-      match (a, b) {
-        (Some(v1), Some(v2)) => {
-          v.push((v1.val+v2.val+c)%10);
-          c = if (v1.val+v2.val+c)>=10 { 1 } else { 0 };
-        },
-        (None, Some(v2)) => {
-          v.push((v2.val+c)%10);
-          c = if (v2.val+c)>=10 { 1 } else { 0 };
-        },
-        (Some(v1), None) => {
-          v.push((v1.val+c)%10);
-          c = if (v1.val+c)>=10 { 1 } else { 0 };
-        },
-        (None, None) => {
-          if c == 1 {
-            v.push(c);
-          }
-          break ();
+    fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut ptr = SinglyLinkedList::new();
+        let mut v = Vec::new();
+        let mut a = l1.as_deref().take();
+        let mut b = l2.as_deref().take();
+        let mut c: i32 = 0;
+        loop {
+            match (a, b) {
+                (Some(v1), Some(v2)) => {
+                    v.push((v1.val+v2.val+c)%10);
+                    c = if (v1.val+v2.val+c)>=10 { 1 } else { 0 };
+                },
+                (None, Some(v2)) => {
+                    v.push((v2.val+c)%10);
+                    c = if (v2.val+c)>=10 { 1 } else { 0 };
+                },
+                (Some(v1), None) => {
+                    v.push((v1.val+c)%10);
+                    c = if (v1.val+c)>=10 { 1 } else { 0 };
+                },
+                (None, None) => {
+                    if c == 1 {
+                        v.push(c);
+                    }
+                    break ();
+                }
+            }
+            a = match a.as_mut() {
+                Some(node) => {
+                    node.next.as_deref().take()
+                },
+                None => None
+            };
+            b = match b.as_mut() {
+                Some(node) => {
+                    node.next.as_deref().take()
+                },
+                None => None
+            };
         }
-      }
-      a = match a.as_mut() {
-        Some(node) => {
-          node.next.as_deref().take()
-        },
-        None => None
-      };
-      b = match b.as_mut() {
-        Some(node) => {
-          node.next.as_deref().take()
-        },
-        None => None
-      };
+        v.reverse();
+        for i in v.iter() {
+            ptr.push(*i);
+        }
+        return ptr.head;
     }
-    v.reverse();
-    for i in v.iter() {
-      ptr.push(*i);
-    }
-    return ptr.head;
-  }
 }
