63619b91c51c0be83c35b6ff7fbd55a681aa5d4b
wiasliaw <leewei48750@gmail.com>
2021-01-24 22:40:38 +0800

    feat(leetcode/q1): feat q1 solution

--- leetcode/src/problem/mod.rs
+++ leetcode/src/problem/mod.rs
@@ -1,2 +1,3 @@
+pub mod q1_two_sum;
 pub mod q2_add_two_numbers;
 pub mod q3_longest_substring_without_repeating_characters;
---
+++ leetcode/src/problem/q1_two_sum.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut mp: HashMap<i32, i32> = HashMap::new();
+        let mut vecc: Vec<i32> = Vec::new();
+        for (k_n, v_n) in nums.iter().enumerate() {
+            if let Some((need, k)) = mp.get_key_value(&v_n) {
+                vecc.extend_from_slice(&[*k, k_n as i32]);
+                break;
+            } else {
+                mp.insert(target - v_n, k_n as i32);
+            }
+        };
+        vecc
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        let n: Vec<i32> = vec![2, 7, 11, 15];
+        let t = 9;
+        println!("{:?}", Solution::two_sum(n, t));
+    }
+}
